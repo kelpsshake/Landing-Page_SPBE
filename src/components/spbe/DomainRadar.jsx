0306d8bdd89437816ac2e488a7e5d422950dfc25
Bunga Devina Firdaus <bungadevina4@gmail.com>
2025-07-27 16:59:54 +0700

    update features

--- src/components/spbe/DomainRadar.jsx
+++ src/components/spbe/DomainRadar.jsx
@@ -10,16 +10,44 @@ import {
 
 export default function DomainRadar({ data }) {
   return (
-    <div style={{ width: '100%', height: 300 }}>
+    <div style={{ width: '100%', height: 350 }}>
       <ResponsiveContainer>
         <RadarChart data={data}>
           <PolarGrid />
-          <PolarAngleAxis dataKey="name" />
+          <PolarAngleAxis dataKey="name" tick={CustomTick}/>
           <PolarRadiusAxis domain={[0, 5]} />
           <Tooltip />
-          <Radar dataKey="value" stroke="#8B5CF6" fill="#8B5CF6" fillOpacity={0.6} />
+          <Radar dataKey="value" stroke="#6fa3f7" fill="#6fa3f7" fillOpacity={0.6} />
         </RadarChart>
       </ResponsiveContainer>
     </div>
   );
 }
+
+const CustomTick = ({ payload, x, y, textAnchor }) => {
+  const words = payload.value.split(' ');
+  const lines = [];
+
+  // Gabung per 2-3 kata agar nggak terlalu panjang ke bawah
+  for (let i = 0; i < words.length; i += 3) {
+    lines.push(words.slice(i, i + 3).join(' '));
+  }
+
+  return (
+    <text
+      x={x}
+      y={y}
+      textAnchor={textAnchor}
+      dominantBaseline="central"
+      fontSize={10}
+      fill="#333"
+    >
+      {lines.map((line, index) => (
+        <tspan x={x} dy={index === 0 ? 0 : 10} key={index}>
+          {line}
+        </tspan>
+      ))}
+    </text>
+  );
+};
+
